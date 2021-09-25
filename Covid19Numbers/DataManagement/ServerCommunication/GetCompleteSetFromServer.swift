4e73c064a0852ac5bcecc1ce27d3d0e516f80fc8
vesan66 <vesan@gmx.de>
2021-09-25 21:16:59 +0200

    Don't break on zero results.

--- Covid19Numbers/DataManagement/ServerCommunication/GetCompleteSetFromServer.swift
+++ Covid19Numbers/DataManagement/ServerCommunication/GetCompleteSetFromServer.swift
@@ -16,9 +16,14 @@ public final class GetCompleteSetFromServer: NSObject {
     
     public static func getDataAwait() -> returnTuple {
         Logger.funcStart.notice("getDataAwait")
+        
+        let magicValueCounties: Int = 412
+        
+        // Init empties
         var cases = CovidCaseS()
         var casesRecoveredData = GetCovidRecoveredDataFromServer.RecoveredCaseS()
         var casesNewData = GetCovidNewDataFromServer.NewCaseS()
+        
         var result = true
         
         // Get the Data
@@ -27,8 +32,10 @@ public final class GetCompleteSetFromServer: NSObject {
             let casesBaseData = getBaseData.getDataAwait()
             Logger.data.notice("Got base items. Count = \(casesBaseData.count, privacy: .public)")
             
-            result = casesBaseData.count > 0
-            cases = casesBaseData
+            result = ((casesBaseData.count > 0) && (casesBaseData.count == magicValueCounties))
+            if result {
+                cases = casesBaseData
+            }
         }
         
         if result {
@@ -45,20 +52,24 @@ public final class GetCompleteSetFromServer: NSObject {
         
         // Merge the Data
         // Recovered and new
-        for index in (0...(cases.count - 1)) {
-            if let recovered = casesRecoveredData[cases[index].idLandkreis] {
-                if recovered.datetime1000 == cases[index].datetime1000 {
-                    cases[index].genesen = recovered.genesen
+        if (cases.count > 0) {
+            for index in (0...(cases.count - 1)) {
+                if let recovered = casesRecoveredData[cases[index].idLandkreis] {
+                    if recovered.datetime1000 == cases[index].datetime1000 {
+                        cases[index].genesen = recovered.genesen
+                    }
                 }
-            }
-            if let newcase = casesNewData[cases[index].idLandkreis] {
-                if newcase.datetime1000 == cases[index].datetime1000 {
-                    cases[index].newCases = newcase.newCases
-                    if cases[index].EWZ > 0 {
-                        cases[index].newCases_per_100k = Double(newcase.newCases) / Double(cases[index].EWZ) * Double(100_000)
+                if let newcase = casesNewData[cases[index].idLandkreis] {
+                    if newcase.datetime1000 == cases[index].datetime1000 {
+                        cases[index].newCases = newcase.newCases
+                        if cases[index].EWZ > 0 {
+                            cases[index].newCases_per_100k = Double(newcase.newCases) / Double(cases[index].EWZ) * Double(100_000)
+                        }
                     }
                 }
             }
+        } else {
+            Logger.data.notice("Got no data.")
         }
         
         
